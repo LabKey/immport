7006c771f20cc0b308fec4ab0e8fc5db3e7428cf
Adam Rauch <adam@labkey.com>
2022-03-14 16:59:32 -0700

    Delete scripts that will never execute (#61)

--- resources/schemas/dbscripts/postgresql/immport-15.34-15.35.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE immport.fcs_analyzed_result DROP COLUMN IF EXISTS pop_cell_number_unit_preferred;
-ALTER TABLE immport.fcs_analyzed_result ADD COLUMN pop_cell_number_unit_preferred VARCHAR(50);
