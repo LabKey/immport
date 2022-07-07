bc75506d9777d8f9804c4366143be0269ee4fd4a
Ian Sigmon <labkey-ians@users.noreply.github.com>
2022-07-07 16:12:22 -0700

    Issue #45834: Extend 'Condition' column width (#72)

--- src/org/labkey/immport/ImmPortModule.java
+++ src/org/labkey/immport/ImmPortModule.java
@@ -70,7 +70,7 @@ public class ImmPortModule extends DefaultModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 22.005;
+        return 22.006;
     }
 
     @Override
