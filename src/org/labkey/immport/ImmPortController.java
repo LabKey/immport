b945ab055fc0c90afacf7d8cb9713c37524a8b7e
Adam Rauch <adam@labkey.com>
2022-09-20 17:26:20 -0700

    Repackage JSONObject and JSONArray

--- src/org/labkey/immport/ImmPortController.java
+++ src/org/labkey/immport/ImmPortController.java
@@ -21,7 +21,7 @@ import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.json.JSONObject;
+import org.json.old.JSONObject;
 import org.labkey.api.action.Action;
 import org.labkey.api.action.ActionType;
 import org.labkey.api.action.ApiSimpleResponse;
